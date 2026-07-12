c4392483fd6b5b208cbe303043c8522326ba0777
SUDAR2106 <sudararasan@gmail.com>
2026-07-12 20:12:40 +0530

    Update DashboardPage.ts

--- PWLearning/pages/DashboardPage.ts
+++ PWLearning/pages/DashboardPage.ts
@@ -1,16 +1,16 @@
-//DashboardPage.ts
-import { expect, Page } from '@playwright/test';
- 
+//DashboardPage.ts need to update
+import { expect, Page } from "@playwright/test";
+
 export class DashboardPage {
- 
-    constructor(private page: Page) {}
- 
-    async verifyDashboardLoaded() {
-        await expect(this.page).toHaveURL("https://www.prudentplus.in/dashboard/home");
-    }
- 
-    async verifyLoggedInUser(userName: string) {
-        await expect(this.page.getByText(userName)).toBeVisible();
-    }
- 
+  constructor(private page: Page) {}
+
+  async verifyDashboardLoaded() {
+    await expect(this.page).toHaveURL(
+      "https://www.prudentplus.in/dashboard/home",
+    );
+  }
+
+  async verifyLoggedInUser(userName: string) {
+    await expect(this.page.getByText(userName)).toBeVisible();
+  }
 }
